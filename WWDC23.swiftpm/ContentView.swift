14573850bacc03f82469df281213c65afe46116f
Milena Maia <milenamaiaaraujo@gmail.com>
2023-04-12 20:54:03 -0300

    :lipstick: style: adjust on video view frame

--- WWDC23.swiftpm/ContentView.swift
+++ WWDC23.swiftpm/ContentView.swift
@@ -2,9 +2,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VideoView(videoId: "https://www.youtube.com/watch?v=9WvQamsXuqI")
-            .frame(minWidth: 0, maxHeight: UIScreen.main.bounds.height * 0.3)
-            .cornerRadius(12)
-            .padding()
+            VideoView(videoId: "https://www.youtube.com/watch?v=9WvQamsXuqI")
+                .cornerRadius(12)
+                .padding()
+        
     }
 }
